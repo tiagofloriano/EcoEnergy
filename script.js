436c503ffaba7daa69c9aacb793898d7998c5bb9
FX Now Team <fxnowteam@protonmail.com>
2023-08-28 17:52:10 -0300

    Inclusão do sistema solar e outras melhorias
    Adicionado Sol, sistema com painel solar, consumo de energia da casa, e vento.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     const battery = document.getElementById("battery");
     let rotationAngle = 0;
     let rotations = 0;
+    $("#vento").fadeOut(1);
+    setInterval(consumo, 3000);
+    setInterval(cargaSolar, 1000);
 
     function rotateWindmill() {
         rotationAngle += 720; // 360 graus para uma rotação completa
@@ -11,7 +14,38 @@ $(document).ready(function () {
         battery.textContent = `${rotations} W`;
     }
 
+    function consumo() {
+        if (rotations > 0) {
+            rotations = rotations - 1;
+            $("#battery").text(rotations + " W");
+            $(".house span").show();
+            $(".house span").fadeOut(2000);
+        }
+    }
+
+    function cargaSolar() {
+        if ($("#statusSolar").val() == 1) {
+            rotations = rotations + 1;
+            $("#battery").text(rotations + " W");
+        }
+    }
+
     document.addEventListener("click", function () {
         rotateWindmill();
+        $("#vento").fadeIn();
+        $("#vento").fadeOut(5000);
     });
 });
+
+function addSolarPanel() {
+    var cargatexto = $("#battery").text();
+    var separacarga = cargatexto.split(" W");
+    var carga = parseInt(separacarga, 10);
+    if (carga < 50) {
+        alert("Voce precisa ter 50 W para adicionar placas solares!");
+    } else {
+        $("#solarpanel").attr("class", "solarpanel");
+        $('#btnAddSolar').hide();
+        $('#statusSolar').val('1');
+    }
+}
